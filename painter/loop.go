f8817b9c41f2015c57f5cf4ffba619eaf455184b
Myroslava Saveniuk <143611799+saveniukk@users.noreply.github.com>
2025-04-18 22:19:49 +0300

    fixed loop.go

--- painter/loop.go
+++ painter/loop.go
@@ -7,12 +7,10 @@ import (
 	"golang.org/x/exp/shiny/screen"
 )
 
-// Receiver отримує текстуру, яка була підготовлена в результаті виконання команд у циклі подій.
 type Receiver interface {
 	Update(t screen.Texture)
 }
 
-// Loop реалізує цикл подій для формування текстури отриманої через виконання операцій отриманих з внутрішньої черги.
 type Loop struct {
 	Receiver Receiver
 
@@ -27,22 +25,31 @@ type Loop struct {
 
 var size = image.Pt(800, 800)
 
-// Start запускає цикл подій. Цей метод потрібно запустити до того, як викликати на ньому будь-які інші методи.
 func (l *Loop) Start(s screen.Screen) {
 	l.next, _ = s.NewTexture(size)
 	l.prev, _ = s.NewTexture(size)
 
 	l.MsgQueue = messageQueue{}
+	l.stopped = make(chan struct{})
+
 	go l.eventProcess()
 }
 
 func (l *Loop) eventProcess() {
 	for {
-		if op := l.MsgQueue.Pull(); op != nil {
-			if update := op.Do(l.next); update {
-				l.Receiver.Update(l.next)
-				l.next, l.prev = l.prev, l.next
-			}
+		op := l.MsgQueue.Pull()
+		if op == nil {
+			continue
+		}
+
+		if update := op.Do(l.next); update {
+			l.Receiver.Update(l.next)
+			l.next, l.prev = l.prev, l.next
+		}
+
+		if l.stopReq {
+			close(l.stopped)
+			return
 		}
 	}
 }
@@ -69,7 +76,9 @@ type messageQueue struct {
 func (mq *messageQueue) Push(op Operation) {
 	mq.mu.Lock()
 	defer mq.mu.Unlock()
+
 	mq.Queue = append(mq.Queue, op)
+
 	if mq.blocked != nil {
 		close(mq.blocked)
 		mq.blocked = nil
@@ -78,15 +87,16 @@ func (mq *messageQueue) Push(op Operation) {
 
 func (mq *messageQueue) Pull() Operation {
 	mq.mu.Lock()
-	defer mq.mu.Unlock()
 	for len(mq.Queue) == 0 {
 		mq.blocked = make(chan struct{})
+		blocked := mq.blocked
 		mq.mu.Unlock()
-		<-mq.blocked
+		<-blocked
 		mq.mu.Lock()
 	}
 	op := mq.Queue[0]
 	mq.Queue[0] = nil
 	mq.Queue = mq.Queue[1:]
+	mq.mu.Unlock()
 	return op
 }
